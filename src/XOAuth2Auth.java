f4817a3aaa8ac4a486239f8c73846c0303b584e2
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-05-24 18:36:10 -0700

    Auth state read

--- src/XOAuth2Auth.java
+++ src/XOAuth2Auth.java
@@ -20,6 +20,27 @@ public class XOAuth2Auth implements Auth{
 	private static void readState(){
 		try{
 			Scanner s = new Scanner(STATE_FILE, "UTF-8");
+			
+			while(s.hasNextLine()){
+				String line = s.nextLine();
+				
+				if(!(line.length() == 0 || line.charAt(0) == '#')){
+					String[] kv = line.split("=" , 2);
+					
+					if(kv.length > 1)
+						state.put(kv[0], kv[1]);
+					else
+						state.put(kv[0], "");
+				}
+				
+			}
+			
+			s.close();
+			
+			if(!(state.containsKey(CID) && state.containsKey(CSEC))){
+				System.err.println("Auth info file is missing needed fields. See README for info");
+				System.exit(ERR_AUTH_INFO_INCOMPLETE);
+			}
 		}
 		catch(IOException e){
 			System.err.println("Auth info file not found. See README for info");
