2aaa8d1200f8aab603d4b6628407f69469a1694b
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-05-26 00:21:41 -0700

    Auth Subprocess

--- src/XOAuth2Auth.java
+++ src/XOAuth2Auth.java
@@ -36,9 +36,37 @@ public class XOAuth2Auth implements Auth{
 		
 		String authToken = null;
 		
+		ProcessBuilder pb = new ProcessBuilder("python3", "auth.py").redirectError(ProcessBuilder.Redirect.INHERIT);
 		
-		
-		return Base64.getEncoder().encode(String.format(FMT, user, authToken).getBytes());
+		try{
+			Process p = pb.start();
+			
+			InputStream os = p.getInputStream();
+			
+			do{
+				try{
+					p.waitFor();
+				}
+				catch(InterruptedException e){}
+			}while(p.isAlive());
+			
+			int ret = p.exitValue();
+			
+			if(ret != 0){
+				System.err.printf("An error occurred with the subprocess (0x%x)\n", ret);
+				System.exit(ERR_AUTH_SUBPROC_FAILED);
+			}
+			
+			authToken = new Scanner(os).nextLine();
+			
+			return Base64.getEncoder().encode(String.format(FMT, user, authToken).getBytes());
+		}
+		catch(Exception e){
+			System.err.println("An error occurred with the subprocess.");
+			System.exit(ERR_AUTH_SUBPROC_FAILED);
+			
+			return null;
+		}
 	}
 	
 	public XOAuth2Auth(){
