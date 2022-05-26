f4ab99f83b1c8257f25175b901837305be9c3ee3
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-05-25 17:36:44 -0700

    Access token script
    Doesn't seem to use refresh token and requires reauthorization in browser but a decent stopgap solution for now
    
    TODO:
    -Adapt output to be handled by main program
    -Stop requiring authorization each run

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,34 @@
-# Module that reads in state info (CID, CSEC, Most recent Auth/Acc token) and returns a valid acc token to parent through stdout
+# From https://gist.github.com/clarketm/dc5d5be390e3f811a2dd7f5e8c5728ba
 
+import os,sys
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import run_flow
+from oauth2client.file import Storage
 
+def disable_stout():
+	o_stdout = sys.stdout
+	o_file = open(os.devnull, "w")
+	sys.stdout = o_file
+	return (o_stdout, o_file)
+
+def enable_stout(o_stdout, o_file):
+	o_file.close()
+	sys.stdout = o_stdout
+
+def getToken():
+	CLIENT_ID = ''
+	CLIENT_SECRET = ''
+	SCOPE = 'https://www.googleapis.com/auth/gmail.send'
+	REDIRECT_URL = 'http://127.0.0.1'
+	
+	o_stdout, o_file = disable_stout()
+	
+	flow = OAuth2WebServerFlow(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=SCOPE, redirect_uri=REDIRECT_URL)
+	
+	storage = Storage('creds.data')
+	credentials = run_flow(flow, storage)
+	enable_stout(o_stdout, o_file)
+	
+	print ("access_token: %s" % (credentials.access_token))
+	
+getToken()
