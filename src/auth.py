4aa3ba31cb45a235466489eef8054320e459ecfa
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-05-27 01:08:13 -0700

    License info

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,16 @@
-# From https://gist.github.com/clarketm/dc5d5be390e3f811a2dd7f5e8c5728ba
+#Copyright (C) 2022  Riley Kuttruff
+#
+#	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#	Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
+# Based on https://gist.github.com/clarketm/dc5d5be390e3f811a2dd7f5e8c5728ba
 
 import os,sys
 
