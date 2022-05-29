64126abbf51a21f9cddc4a34bf09972ee5c538f0
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-05-28 23:01:49 -0700

    Tab to Space

--- src/Auth.java
+++ src/Auth.java
@@ -1,19 +1,19 @@
 /*Copyright (C) 2022  Riley Kuttruff
 *
-*	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
+*   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 *
-*	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-*	Public License for more details.
+*   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+*   Public License for more details.
 *
-*	You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*   You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 */
 
 public interface Auth{
-	static final int ERR_AUTH_INFO_NOT_FOUND = 0x9;
-	static final int ERR_AUTH_INFO_INCOMPLETE = 0xa;
-	static final int ERR_AUTH_SUBPROC_FAILED = 0xb;
-	
-	public byte[] buildAuthString(String user);
+    static final int ERR_AUTH_INFO_NOT_FOUND = 0x9;
+    static final int ERR_AUTH_INFO_INCOMPLETE = 0xa;
+    static final int ERR_AUTH_SUBPROC_FAILED = 0xb;
+    
+    public byte[] buildAuthString(String user);
 }
--- src/XOAuth2Auth.java
+++ src/XOAuth2Auth.java
@@ -1,12 +1,12 @@
 /*Copyright (C) 2022  Riley Kuttruff
 *
-*	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
+*   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 *
-*	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-*	Public License for more details.
+*   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+*   Public License for more details.
 *
-*	You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*   You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 */
 
@@ -14,124 +14,124 @@ import java.util.*;
 import java.io.*;
 
 public class XOAuth2Auth implements Auth{
-	
-	private static final String[] INT_CMD = {"python3", "auth.py"};
-	private static final String[] EXE_CMD = {"auth.exe"};
-	
-	private static String[] getCmd(){
-		try{
-			String pathString = System.getenv("PATH");
-		
-			String[] dirs = pathString.split(System.getProperty("path.separator"));
-			
-			for(String dir : dirs){
-				File d = new File(dir);
-				
-				if(d.listFiles((a) -> {return a.getName().contains("python3");}).length > 0)
-					return INT_CMD;
-			}
-			
-		}
-		catch(Exception e){}
-		
-		if(!(new File("auth.exe").exists() || new File("auth").exists())){
-			try{
-				File exe = new File("auth.exe");
-				
-				exe.deleteOnExit();
-				
-				InputStream is = XOAuth2Auth.class.getResource("auth.exe").openStream();
-				OutputStream os = new FileOutputStream(new File("auth.exe"));
-				
-				byte[] buf = new byte[4096];
-				int len;
-				
-				while((len = is.read(buf)) != -1)
-					os.write(buf, 0, len);
-				
-				is.close();
-				os.close();
-			}
-			catch(IOException e){
-				System.err.println("An IO error occurred trying to extract the auth module");
-				System.exit(SMTPClient.ERR_IO_ERROR);
-			}
-		}
-			
-		return EXE_CMD;
-		
-	}
-	
-	private boolean verifyEnvFile(){
-		File envfile = new File(".env");
-		
-		if(!envfile.exists()){
-			System.err.println(".env file not found. See README for info");
-			System.exit(ERR_AUTH_INFO_NOT_FOUND);
-		}
-		
-		boolean id = false, sec = false;
-		
-		try{
-			Scanner s = new Scanner(envfile);
-			
-			while(s.hasNextLine()){
-				String line = s.nextLine();
-				
-				id = id || line.startsWith("CLIENT_ID");
-				sec = sec || line.startsWith("CLIENT_SECRET");
-			}
-			
-			s.close();
-		}
-		catch(Exception e) {}
-		
-		return id && sec;
-	}
-	
-	@Override
-	public byte[] buildAuthString(String user){
-		final String FMT = "user=%s\001auth=Bearer %s\001\001";
-		
-		String authToken = null;
-		
-		ProcessBuilder pb = new ProcessBuilder(getCmd()).redirectError(ProcessBuilder.Redirect.INHERIT);
-		
-		try{
-			Process p = pb.start();
-			
-			InputStream os = p.getInputStream();
-			
-			do{
-				try{
-					p.waitFor();
-				}
-				catch(InterruptedException e){}
-			}while(p.isAlive());
-			
-			int ret = p.exitValue();
-			
-			if(ret != 0){
-				System.err.printf("An error occurred with the subprocess (0x%x)\n", ret);
-				System.exit(ERR_AUTH_SUBPROC_FAILED);
-			}
-			
-			authToken = new Scanner(os).nextLine();
-			
-			return Base64.getEncoder().encode(String.format(FMT, user, authToken).getBytes());
-		}
-		catch(Exception e){
-			System.err.println("An error occurred with the subprocess.");
-			System.exit(ERR_AUTH_SUBPROC_FAILED);
-			
-			return null;
-		}
-	}
-	
-	public XOAuth2Auth(){
-		if(!verifyEnvFile()){
-			System.err.println("Auth info file is missing needed fields. See README for info");
-			System.exit(ERR_AUTH_INFO_INCOMPLETE);
-		}
-	}
+    
+    private static final String[] INT_CMD = {"python3", "auth.py"};
+    private static final String[] EXE_CMD = {"auth.exe"};
+    
+    private static String[] getCmd(){
+        try{
+            String pathString = System.getenv("PATH");
+        
+            String[] dirs = pathString.split(System.getProperty("path.separator"));
+            
+            for(String dir : dirs){
+                File d = new File(dir);
+                
+                if(d.listFiles((a) -> {return a.getName().contains("python3");}).length > 0)
+                    return INT_CMD;
+            }
+            
+        }
+        catch(Exception e){}
+        
+        if(!(new File("auth.exe").exists() || new File("auth").exists())){
+            try{
+                File exe = new File("auth.exe");
+                
+                exe.deleteOnExit();
+                
+                InputStream is = XOAuth2Auth.class.getResource("auth.exe").openStream();
+                OutputStream os = new FileOutputStream(new File("auth.exe"));
+                
+                byte[] buf = new byte[4096];
+                int len;
+                
+                while((len = is.read(buf)) != -1)
+                    os.write(buf, 0, len);
+                
+                is.close();
+                os.close();
+            }
+            catch(IOException e){
+                System.err.println("An IO error occurred trying to extract the auth module");
+                System.exit(SMTPClient.ERR_IO_ERROR);
+            }
+        }
+            
+        return EXE_CMD;
+        
+    }
+    
+    private boolean verifyEnvFile(){
+        File envfile = new File(".env");
+        
+        if(!envfile.exists()){
+            System.err.println(".env file not found. See README for info");
+            System.exit(ERR_AUTH_INFO_NOT_FOUND);
+        }
+        
+        boolean id = false, sec = false;
+        
+        try{
+            Scanner s = new Scanner(envfile);
+            
+            while(s.hasNextLine()){
+                String line = s.nextLine();
+                
+                id = id || line.startsWith("CLIENT_ID");
+                sec = sec || line.startsWith("CLIENT_SECRET");
+            }
+            
+            s.close();
+        }
+        catch(Exception e) {}
+        
+        return id && sec;
+    }
+    
+    @Override
+    public byte[] buildAuthString(String user){
+        final String FMT = "user=%s\001auth=Bearer %s\001\001";
+        
+        String authToken = null;
+        
+        ProcessBuilder pb = new ProcessBuilder(getCmd()).redirectError(ProcessBuilder.Redirect.INHERIT);
+        
+        try{
+            Process p = pb.start();
+            
+            InputStream os = p.getInputStream();
+            
+            do{
+                try{
+                    p.waitFor();
+                }
+                catch(InterruptedException e){}
+            }while(p.isAlive());
+            
+            int ret = p.exitValue();
+            
+            if(ret != 0){
+                System.err.printf("An error occurred with the subprocess (0x%x)\n", ret);
+                System.exit(ERR_AUTH_SUBPROC_FAILED);
+            }
+            
+            authToken = new Scanner(os).nextLine();
+            
+            return Base64.getEncoder().encode(String.format(FMT, user, authToken).getBytes());
+        }
+        catch(Exception e){
+            System.err.println("An error occurred with the subprocess.");
+            System.exit(ERR_AUTH_SUBPROC_FAILED);
+            
+            return null;
+        }
+    }
+    
+    public XOAuth2Auth(){
+        if(!verifyEnvFile()){
+            System.err.println("Auth info file is missing needed fields. See README for info");
+            System.exit(ERR_AUTH_INFO_INCOMPLETE);
+        }
+    }
 }
