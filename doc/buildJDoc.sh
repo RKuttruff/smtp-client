f1c69648b2193ec2ea93a74edc487bd137bbfe1e
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-05-31 02:44:52 -0700

    Update buildJDoc.sh

--- doc/buildJDoc.sh
+++ doc/buildJDoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ../src/ -type f -name "*.java" | xargs javadoc -private
+find ../src/ -type f -name "*.java" | xargs javadoc -private  -link https://docs.oracle.com/en/java/javase/11/docs/api/ -d javadoc
